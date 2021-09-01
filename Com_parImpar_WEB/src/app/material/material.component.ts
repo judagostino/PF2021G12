51be1ee286f0d3b56006b09ab3757141f34558ac
judagostino <judagostino96@gmail.com>
2021-08-31 23:02:06 -0300

    navbar con busqueda ok

--- Com_parImpar_WEB/src/app/material/material.component.ts
+++ Com_parImpar_WEB/src/app/material/material.component.ts
@@ -1,16 +1,18 @@
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field'; 
 import { NgModule } from '@angular/core';
-
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   imports: [
     MatInputModule,
     MatFormFieldModule,
+    MatIconModule,
   ],
   exports: [
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
