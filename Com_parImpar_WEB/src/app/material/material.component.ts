3071fcab1872d8b668d147ab1ced389b275a01f7
macaVaca <macarenavaca.mv@gmail.com>
2021-09-06 23:03:18 -0300

    Register front

--- Com_parImpar_WEB/src/app/material/material.component.ts
+++ Com_parImpar_WEB/src/app/material/material.component.ts
@@ -2,17 +2,33 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field'; 
 import { NgModule } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import {MatCardModule} from '@angular/material/card';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
 
 @NgModule({
   imports: [
     MatInputModule,
     MatFormFieldModule,
     MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   exports: [
     MatInputModule,
     MatFormFieldModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatCardModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class MaterialModule { }
