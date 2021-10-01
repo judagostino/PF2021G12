11868ba9859b1a626c8ae82ba22de047acba123a
judagostino <judagostino96@gmail.com>
2021-09-30 22:29:31 -0300

    front calendar

--- Com_parImpar_WEB/src/app/pages/calendar/calendar.component.ts
+++ Com_parImpar_WEB/src/app/pages/calendar/calendar.component.ts
@@ -4,11 +4,20 @@ import { EventsService } from 'src/app/services';
 import * as moment from 'moment';
 
 
+// export interface PeriodicElement {
+//   name: string;
+//   position: number;
+//   weight: number;
+//   symbol: string;
+// } 
+
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
+
+
 export class CalendarComponent implements OnInit {
   events: Events[] = [];
   calendarDates: number[] = [];
@@ -105,3 +114,61 @@ export class CalendarComponent implements OnInit {
     this.createCalendar(this.now);
   }
 }
+
+//TABLA MATERIAL NO FUNCIONA
+//TS
+// export class TableRowBindingExample {
+//   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+//   dataSource = ELEMENT_DATA;
+//   clickedRows = new Set<PeriodicElement>();
+// }
+// const ELEMENT_DATA: PeriodicElement[] = [
+//   {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+//   {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+//   {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+//   {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+//   {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+//   {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+//   {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+//   {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+//   {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+//   {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+// ];
+
+//HTML
+// <div class="mat-elevation-z8">
+//                                     <table mat-table [dataSource]="dataSource">
+                                  
+//                                       <!-- Position Column -->
+//                                       <ng-container matColumnDef="position">
+//                                         <th mat-header-cell *matHeaderCellDef> No. </th>
+//                                         <td mat-cell *matCellDef="let element"> {{element.position}} </td>
+//                                       </ng-container>
+                                  
+//                                       <!-- Name Column -->
+//                                       <ng-container matColumnDef="name">
+//                                         <th mat-header-cell *matHeaderCellDef> Name </th>
+//                                         <td mat-cell *matCellDef="let element"> {{element.name}} </td>
+//                                       </ng-container>
+                                  
+//                                       <!-- Weight Column -->
+//                                       <ng-container matColumnDef="weight">
+//                                         <th mat-header-cell *matHeaderCellDef> Weight </th>
+//                                         <td mat-cell *matCellDef="let element"> {{element.weight}} </td>
+//                                       </ng-container>
+                                  
+//                                       <!-- Symbol Column -->
+//                                       <ng-container matColumnDef="symbol">
+//                                         <th mat-header-cell *matHeaderCellDef> Symbol </th>
+//                                         <td mat-cell *matCellDef="let element"> {{element.symbol}} </td>
+//                                       </ng-container>
+                                  
+//                                       <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+//                                       <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
+//                                     </table>
+                                  
+//                                     <mat-paginator [pageSizeOptions]="[5, 10, 20]"
+//                                                    showFirstLastButtons 
+//                                                    aria-label="Select page of periodic elements">
+//                                     </mat-paginator>
+//                                   </div>
