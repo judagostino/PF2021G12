c8b4ce2f0c0a9a9d82807501e80ec277cdef20cf
judagostino <43083482+judagostino@users.noreply.github.com>
2021-10-13 20:11:55 -0300

    navbar ocultar menu

--- Com_parImpar_WEB/src/app/services/config.service.ts
+++ Com_parImpar_WEB/src/app/services/config.service.ts
@@ -1,26 +1,36 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map, mergeMap } from 'rxjs/operators';
+import { ContactService } from './contact.service';
+import { Contact } from '../intrergaces';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class ConfigService {
   public isLooged:boolean = false;
+  public contact:Contact = null;
 
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, private contactService:ContactService) {
 
   }
 
   public load(): Promise<void> {
-    // return new Promise((resolve) => {
-    //   this.reAuthenticate().pipe(
-    //     mergeMap((isLooged: boolean) => {
-    //       this.isLooged = isLooged;
-    //       return of(isLooged);
-    //     })
-    //   ).subscribe((loadData) => resolve());
-    // });
-    return new Promise((resolve) => resolve(null))
+    return new Promise((resolve) => {
+      this.reAuthenticate().pipe(
+        mergeMap((isLooged: boolean) => {
+          this.isLooged = isLooged;
+          if(isLooged){
+            return this.contactService.myInfo().pipe(map(res =>{
+              this.contact = res;
+              return true;
+            }))
+          } else {
+            return of(isLooged);
+          }
+        })
+      ).subscribe((loadData) => resolve());
+    });
+    //return new Promise((resolve) => resolve(null))
   }
 
   private reAuthenticate(): Observable<boolean> {
